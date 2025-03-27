fac528a9d1289e7af2e51be696e7733dc4ec91ae
AJ-cubes <jaina10@wis.edu.hk>
2025-03-27 19:46:05 +0800

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-let dictionary = {
-  "Team 1": 3,
-  "Team 2": 1,
-  "Team 3": 5,
-  "Team 4": 2
-};
-
-dictionary = Object.fromEntries(Object.entries(dictionary).sort((a, b) => a[1] - b[1]));
-console.log(dictionary);
